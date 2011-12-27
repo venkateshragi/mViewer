92010682ef8b563774967a878cb0aea2dd97eaab
Kumar Varun <k.varun31@gmail.com>
2011-12-27 15:42:47 +0530

    Correctred Batch File Fix the errors in Batch file to start the app

--- scripts/start_mviewer.bat
+++ scripts/start_mviewer.bat
@@ -1,12 +1,14 @@
-if [%1]==[](
-  For /F "tokens=1* delims==" %%A IN (mViewer.properties) DO (
-	IF "%%A"=="WINSTONE_PORT" set port=%%B
+echo off
+IF [%1]==[] (
+  FOR /F "tokens=1* delims==" %%A IN (mViewer.properties) DO (
+    IF "%%A"=="WINSTONE_PORT" set port=%%B
         )
   ECHO Using Default Http Port %port%
-) ELSE(
-  port=%1
-  ECHO Using Http Port %port%
+) ELSE (
+  set port=%1
+  ECHO Using Http Port %1
 )
-
+echo on
 java -jar winstone-0.9.10.jar --httpPort=%port% --ajp13Port=-1 --warfile=mViewer.war
+
 
