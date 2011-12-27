9c0a1a8337a3ae62e8bc9e626e000fcb34c26cf9
Kumar Varun <k.varun31@gmail.com>
2011-12-27 17:41:01 +0530

    Modified Version in Build, Updated Shell script Build has now modigfied version to cater v0.8 Also modified shell script and remove if-else with inline substitution.

--- scripts/start_mviewer.sh
+++ scripts/start_mviewer.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-if [ $1 != "" ]
-then
-   port=$1
-else
-   . mViewer.properties
-   port=$WINSTONE_PORT
-fi
+. mViewer.properties
+port=${1:-$WINSTONE_PORT}
+echo Using Http Port : $port
 java -jar ./winstone-0.9.10.jar --httpPort=$port --ajp13Port=-1 --warfile=./mViewer.war
